abc64a565421c7c5db223dd3175e819020858dda
Andy Whyte <andrew@photon.paramita-electronics.org>
2023-07-10 12:55:53 +0100

    added origin for setup.py file

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 from setuptools import setup, find_packages
 
+# based on: https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder/50194143#50194143
 setup(name='diffgerber', version='0.1.1', packages=find_packages())
